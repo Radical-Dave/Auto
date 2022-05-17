7b40ea3e33893c71ac98cb17aa10f7e623759398
David Walker <dave@radicaldave.com>
2022-05-17 09:10:06 -0500

    ouput param update

--- data/providers/tf/tasks/resourcegroup/output.tf
+++ data/providers/tf/tasks/resourcegroup/output.tf
@@ -1,8 +1,8 @@
 output "ResourceGroupId" {
   description = "The id of the resource group provisioned"
-  value = "${module.azurerm_resource_group.id}"
+  value = module.azurerm_resource_group.id
 }
 output "ResourceGroupName" {
   description = "The name of the resource group provisioned"
-  value = "${module.azurerm_resource_group.name}"
+  value = module.azurerm_resource_group.name
 }
--- data/providers/tf/tasks/terraform/output.tf
+++ data/providers/tf/tasks/terraform/output.tf
@@ -1,8 +1,8 @@
 output "ResourceGroupId" {
   description = "The id of the resource group provisioned"
-  value = "${azurerm_resource_group.rg.id}"
+  value = azurerm_resource_group.rg.id
 }
 output "ResourceGroupName" {
   description = "The name of the resource group provisioned"
-  value = "${azurerm_resource_group.rg.name}"
+  value = azurerm_resource_group.rg.name
 }
